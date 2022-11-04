a72af8e574cf6887692c7b17fd64f01801032a08
Guilherme Nunes <guilhermenunes@Guilhermes-MacBook-Air.local>
2022-11-04 16:56:04 -0300

    Register form(Reactive form) + Login form(Template form)

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
-import { ModalService } from '../services/modal.service';
 import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 import { TabComponent } from './tab/tab.component';
+import { InputComponent } from './input/input.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskModule } from 'ngx-mask';
+import { AlertComponent } from './alert/alert.component';
 
 @NgModule({
-  declarations: [ModalComponent, TabsContainerComponent, TabComponent],
-  imports: [CommonModule],
-  exports: [ModalComponent, TabsContainerComponent, TabComponent],
-  providers: [ModalService],
+  declarations: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent,
+    InputComponent,
+    AlertComponent,
+  ],
+  imports: [CommonModule, ReactiveFormsModule, NgxMaskModule.forRoot()],
+  exports: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent,
+    InputComponent,
+    AlertComponent,
+  ],
+  providers: [],
 })
 export class SharedModule {}
